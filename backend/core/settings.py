6691060f16d1ff8badce95459fca0f7925dab83d
Akshad <akshadtupe0@gmail.com>
2026-03-10 16:43:35 +0530

    Add production database config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,14 +95,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "cvsync",
-        "USER": "postgres",
-        "PASSWORD": "2409",
-        "HOST": "postgres",  # Use the service name defined in docker-compose.yml
-        "PORT": "5432",
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
