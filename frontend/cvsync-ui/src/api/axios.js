f164ddc71850fb27a87b8c110dfd44fc18ec7760
Akshad <akshadtupe0@gmail.com>
2026-03-14 11:59:35 +0530

    prepare for vercel deployment

--- frontend/cvsync-ui/src/api/axios.js
+++ frontend/cvsync-ui/src/api/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
-  baseURL: "https://cvsync-1.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
 
-  // no attach token for register or login
+  //no attach token for register or login
   if (
     token &&
     !config.url.includes("register") &&
