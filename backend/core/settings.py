25e1831e1ef13a2dca34eaffc86633d7fbe0e75b
Akshad <akshadtupe0@gmail.com>
2026-03-14 13:36:52 +0530

    ready for vercel deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,11 +80,13 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://cv-sync.vercel.app/",
+
+]
 
 
 ROOT_URLCONF = "core.urls"
